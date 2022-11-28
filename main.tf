f88848f3dae15167abbe36ae5b44f49633a347ee
nicksgithub99 <110228098+nicksgithub99@users.noreply.github.com>
2022-11-27 23:15:02 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,92 @@
+#
+#
+#-------------------------------------------------------------
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA53O3V4OWRHSNHXNB"
+  secret_key = "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"
+}
+
+#-----------------------
+# EKS CLuster Definition
+#-----------------------
+
+resource "aws_eks_cluster" "eksdemo" {
+  name     = "EKS-cluster"
+  role_arn = aws_iam_role.eksdemorole.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-078d90354c8c6ebe5", "subnet-09ec6941e73860d80	"]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eksdemorole-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eksdemorole-AmazonEKSVPCResourceController,
+  ]
+}
+
+
+#-------------------------
+# IAM Role for EKS Cluster
+#-------------------------
+
+resource "aws_iam_role" "eksdemorole" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemorole-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eksdemorole.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemorole-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eksdemorole.name
+}
+
+#--------------------------------------
+# Enabling IAM Role for Service Account
+#--------------------------------------
+
+data "tls_certificate" "ekstls" {
+  url = aws_eks_cluster.eksdemo.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eksopidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.ekstls.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eksdemo.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "eksdoc_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eksopidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eksopidc.arn]
+      type        = "Federated"
+    }
+  }
+}
